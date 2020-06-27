b77d5418ecfa0b7f4937077729e6c6acdd7ded58
luoyanjie <546223592@qq.com>
2020-06-27 13:35:30 +0800

    Thor    1、first commit;

--- web/src/main/java/com/thor/pawPrint/Application.java
+++ web/src/main/java/com/thor/pawPrint/Application.java
@@ -14,6 +14,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan
 public class Application {
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 
